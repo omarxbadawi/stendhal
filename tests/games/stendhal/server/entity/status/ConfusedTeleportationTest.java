9bd3d2594206177d301f7fda1ca99aebcf410623
Jin Wang <jin.wang-12@student.manchester.ac.uk>
2021-10-08 10:47:26 +0100

    changed name of item, created a zone, errors is removed, but test passed

--- tests/games/stendhal/server/entity/status/ConfusedTeleportationTest.java
+++ tests/games/stendhal/server/entity/status/ConfusedTeleportationTest.java
@@ -21,6 +21,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.MockStendlRPWorld;
 import utilities.PlayerTestHelper;
@@ -42,13 +43,15 @@ public class ConfusedTeleportationTest {
 	@Test
 	public void testConfusedTeleport() {
 		final Player victim = PlayerTestHelper.createPlayer("Bob");
+		StendhalRPZone zone = new StendhalRPZone("the zone where the corpse shall be slain");
+		zone.add(victim);
 		final ConfuseStatus Confused = new ConfuseStatus();
 		final ConfuseStatusHandler confuseStatusHandler = new ConfuseStatusHandler();
 		confuseStatusHandler.inflict(Confused, victim.getStatusList(), SingletonRepository.getEntityManager().getCreature("mermaid"));
 		Map<String, String> attribute = new HashMap<String, String>();
 		attribute.put("quantity", "1");
 		attribute.put("menu", "Use|Use");
-		final MarkedScroll Tobeused = new MarkedScroll("Testscroll", "scroll", "marked", attribute);
+		final MarkedScroll Tobeused = new MarkedScroll("marked scroll", "scroll", "marked", attribute);
 		victim.equip("bag", Tobeused);
 		assertFalse(Tobeused.onUsed(victim));
 	}
