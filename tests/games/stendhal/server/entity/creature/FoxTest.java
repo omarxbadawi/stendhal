375ca97664d9d101636145b324e3d00e65584ab0
Georgia May Cawley <georgia.cawley@student.manchester.ac.uk>
2021-11-30 13:00:58 +0000

    Add tests for new stealing pet
    When run, these tests fail as the necessary classes and functions
    haven't been implemented yet. This will guide us in developing the full
    feature.

---
+++ tests/games/stendhal/server/entity/creature/FoxTest.java
@@ -0,0 +1,33 @@
+package games.stendhal.server.entity.creature;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.maps.MockStendlRPWorld;
+//import utilities.RPClass.FoxTestHelper;
+
+public class FoxTest {
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		//FoxTestHelper.generateRPClasses();
+		MockStendlRPWorld.get();
+	}
+	
+	/**
+	 * Tests for babyDragon.
+	 */
+	@Test
+	public void testFox() {
+		Creature swiper = SingletonRepository.getEntityManager().getCreature("fox");
+		int moneyChance = 3;// TODO swiper.getMoneyPercent();
+		assertEquals(0.1, moneyChance, 0.001);
+		
+	}
+
+}
---
+++ tests/utilities/RPClass/FoxTestHelperTest.java
@@ -0,0 +1,17 @@
+package utilities.RPClass;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import marauroa.common.game.RPClass;
+
+public class FoxTestHelperTest {
+
+	@Test
+	public void testGenerateRPClasses() {
+		//TODO
+		//FoxTestHelper.generateRPClasses();
+		assertTrue(RPClass.hasRPClass("fox"));
+	}
+
+}
