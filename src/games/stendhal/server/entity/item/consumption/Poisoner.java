81c996b816117ddb2ee79f05e37c80642a61273b
J46835SS <Shakhawath.Shah@student.manchester.ac.uk>
2021-12-02 13:06:02 +0000

    add sleep potion
    this allows admin users to summon
    a potion for sleeping

--- src/games/stendhal/server/entity/item/consumption/Poisoner.java
+++ src/games/stendhal/server/entity/item/consumption/Poisoner.java
@@ -11,9 +11,14 @@
  ***************************************************************************/
 package games.stendhal.server.entity.item.consumption;
 
+import java.util.Arrays;
+import java.util.List;
+
 import games.stendhal.server.entity.item.ConsumableItem;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.entity.status.PoisonStatus;
+import games.stendhal.server.entity.status.SleepStatus;
+import games.stendhal.server.entity.status.Status;
 
 /**
  * Poisoner
@@ -23,7 +28,13 @@ public class Poisoner implements Feeder {
 	@Override
 	public boolean feed(final ConsumableItem item, final Player player) {
 		ConsumableItem splitOff = (ConsumableItem) item.splitOff(1);
-		PoisonStatus status = new PoisonStatus(splitOff.getAmount(), splitOff.getFrecuency(), splitOff.getRegen());
+		Status status;
+		List<String> sleep = Arrays.asList("sleep potion");
+		if (sleep.contains(item.getName())) {
+			status = new SleepStatus(splitOff.getAmount(), splitOff.getFrecuency(), splitOff.getRegen());
+		} else {
+			status = new PoisonStatus(splitOff.getAmount(), splitOff.getFrecuency(), splitOff.getRegen());
+		}
 		player.getStatusList().inflictStatus(status, splitOff);
 		return true;
 	}
