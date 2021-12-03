71d055d498ffd62a4dcac14576db60ecf9d46994
Ignes Simeonova <ignes.simeonova@student.manchester.ac.uk>
2021-12-03 08:57:36 +0200

    Add basic furniture slot class.

---
+++ src/games/stendhal/server/entity/slot/FurnitureSlot.java
@@ -0,0 +1,18 @@
+package games.stendhal.server.entity.slot;
+
+/**
+ * Slot for furniture.
+ */
+public class FurnitureSlot extends EntitySlot {
+
+	/**
+	 * Creates a new furniture slot.
+	 *
+	 * @param name
+	 *            name of slot
+	 */
+	public FurnitureSlot(final String name) {
+		super(name, name);
+	}
+
+}
