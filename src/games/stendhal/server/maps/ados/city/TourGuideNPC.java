354798443aaa4cd4dfc317fc120c30f24f733188
x86611ia <ingy.abdelhalim@student.manchester.ac.uk>
2021-12-03 03:07:43 +0000

    Let players teleport from ados
    Modify the NPC's speech and allow players to teleport to nalwor, where
    they will meet up with their tour guide their.
    
    Implements: Teleporting in a tour

--- src/games/stendhal/server/maps/ados/city/TourGuideNPC.java
+++ src/games/stendhal/server/maps/ados/city/TourGuideNPC.java
@@ -12,52 +12,51 @@
  ***************************************************************************/
 package games.stendhal.server.maps.ados.city;
 
-import games.stendhal.server.core.config.ZoneConfigurator;
-import games.stendhal.server.core.engine.SingletonRepository;
-import games.stendhal.server.core.engine.StendhalRPZone;
-// this one is just because our NPC is a seller
-import games.stendhal.server.entity.npc.ShopList;
-import games.stendhal.server.entity.npc.SpeakerNPC;
-// this one is just because our NPC is a seller
-//import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
-// this one is just because our NPC is a seller
-//import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
-//import games.stendhal.server.entity.npc.ChatAction;
-//import games.stendhal.server.entity.npc.ConversationStates;
-//import games.stendhal.server.entity.npc.action.MultipleActions;
-//import games.stendhal.server.entity.player.Player;
-//import games.stendhal.server.entity.npc.condition.LevelGreaterThanCondition;
-//import games.stendhal.server.entity.npc.condition.LevelLessThanCondition;
-
-//import java.util.Arrays;
-//import java.util.LinkedList;
-//import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
 
+
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.TeleportAction;
+
 public class TourGuideNPC implements ZoneConfigurator {
-    // this is just because he has a 'shop' to sell tour tickets
-	private final ShopList shops = SingletonRepository.getShopList();
-	
 	/**
 	 * Configure a zone.
 	 *
 	 * @param    zone        The zone to be configured.
 	 * @param    attributes    Configuration attributes.
 	 */
+	@Override
 	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
 	    buildNPC(zone);
 	}
 	private void buildNPC(final StendhalRPZone zone) {
         final SpeakerNPC Touristo = new SpeakerNPC("Touristo Maps") {
+        
+        @Override
         protected void createPath() {
             setPath(null);
         }
 
+        @Override
         protected void createDialog() {
-            // Lets the NPC reply with "Hello"
-            addGreeting("Hi! My name is Touristo. I'll be starting a tour to Nalwor soon.");
-            //NPC's response when a player says goodbye
-            addGoodbye("Bye");
+        	
+//        	final SpeakerNPC Touristo = new SpeakerNPC("Touristo Maps");
+        	
+        	addGreeting("Hi! My name is Tousristo, do you want to go on a tour to Nalwor city?");
+     
+        	add(ConversationStates.ATTENDING, Arrays.asList("ya", "Ya", "yes", "Yes", "yes please", "yah"),
+                    ConversationStates.ATTENDING,
+                    "Great, lets go!", new TeleportAction("0_nalwor_city_n",29,80,null));  
+            
+        	add(ConversationStates.ATTENDING, Arrays.asList("no", "No", "No thanks", "nope"),
+                    ConversationStates.ATTENDING,
+                    "Okay, see you around.", null);
+
         }
     };
 
@@ -66,7 +65,6 @@ public class TourGuideNPC implements ZoneConfigurator {
     // set a description for when a player does 'Look'
     Touristo.setDescription("You see Toursito Maps , he looks a a bit busy at the moment but perhaps he can help you anyway.");
     // Set the initial position to be the first node on the Path you defined above.
-    //che
     Touristo.setPosition(50, 90);
     Touristo.initHP(100);
 
--- tests/games/stendhal/server/maps/ados/city/TourGuideNPCTest.java
+++ tests/games/stendhal/server/maps/ados/city/TourGuideNPCTest.java
@@ -11,11 +11,7 @@
  ***************************************************************************/
 package games.stendhal.server.maps.ados.city;
 
-import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-//import static org.junit.Assert.assertTrue;
-//import static utilities.SpeakerNPCTestHelper.getReply;
+import static org.junit.Assert.*;
 import static utilities.SpeakerNPCTestHelper.getReply;
 
 import org.junit.Before;
@@ -23,67 +19,69 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import games.stendhal.server.core.engine.SingletonRepository;
-import games.stendhal.server.entity.npc.ConversationStates;
-//import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.fsm.Engine;
-//import games.stendhal.server.entity.npc.fsm.Engine;
 import games.stendhal.server.entity.player.Player;
 import utilities.PlayerTestHelper;
 import utilities.QuestHelper;
 import utilities.ZonePlayerAndNPCTestImpl;
 
-public class TourGuideNPCTest extends ZonePlayerAndNPCTestImpl {
-
+public class TourGuideNPCTest extends ZonePlayerAndNPCTestImpl{
+	
 	private static final String ZONE_NAME = "0_ados_city_n";
 	private static final String npcName = "Touristo Maps";
 	private static Player player;
 	private static SpeakerNPC Touristo;
 	private Engine touristoEngine;
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		QuestHelper.setUpBeforeClass();
-		setupZone(ZONE_NAME, new TourGuideNPC());
-	}
-
-	@Override
-	@Before
-	public void setUp() throws Exception {
-		setZoneForPlayer(ZONE_NAME);
-		setNpcNames(npcName);
-
-		super.setUp();
-		player = PlayerTestHelper.createPlayer("player");
-		Touristo = SingletonRepository.getNPCList().get("Touristo Maps");
-
-		touristoEngine = Touristo.getEngine();
-	}
-
-	@Test
-	public void initTest() {
-		TourGuideDialogueTest();
-		testEntities();
-	}
 	
-	public TourGuideNPCTest() {
-		super(ZONE_NAME, "Touristo Maps");
-	}
 
-	//Player and Touristo are not null values
-	private void testEntities() {
-		assertNotNull(player);
-		assertNotNull(Touristo);
-	}
-	
-	private void TourGuideDialogueTest() {
 
-		touristoEngine.step(player, "hi");
-		assertEquals(ConversationStates.ATTENDING, touristoEngine.getCurrentState());
-		assertEquals("Hi! My name is Touristo. I'll be starting a tour to Nalwor soon.", getReply(Touristo));
-		touristoEngine.step(player, "bye");
-		assertEquals("Bye", getReply(Touristo));
-		assertEquals(ConversationStates.IDLE, touristoEngine.getCurrentState());
-	}
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        QuestHelper.setUpBeforeClass();
+        setupZone(ZONE_NAME, new TourGuideNPC());
+    }
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        setZoneForPlayer(ZONE_NAME);
+        setNpcNames(npcName);
+        super.setUp();
+        Touristo = SingletonRepository.getNPCList().get(npcName);
+
+
+    }
+
+    @Test
+    public void DialogueTest() {
+        if(Touristo != null) {
+            touristoEngine = Touristo.getEngine();
+            player = PlayerTestHelper.createPlayer("testPlayer");
+            createDialog();
+            TourDialogueRejectionTest();
+        }else {
+        	assertTrue(false);
+        }
+    }
+    public void createDialog() {
+        touristoEngine.step(player, "hi");
+    	assertEquals("Hi! My name is Tousristo, do you want to go on a tour to Nalwor city?", getReply(Touristo));
+		touristoEngine.step(player, "yes");
+		assertEquals("Great, lets go!", getReply(Touristo));
+		StendhalRPZone newZone = SingletonRepository.getRPWorld().getZone("0_nalwor_city_n");
+	    assertEquals(newZone, player.getZone());
+
+    }
+
+
+    public void TourDialogueRejectionTest() {
+        touristoEngine = Touristo.getEngine();
+        player = PlayerTestHelper.createPlayer("testPlayer");
+//		touristoEngine.step(player, "hi");
+//		assertEquals("Hi! My name is Tousristo, do you want to go on a tour to Nalwor city?", getReply(Touristo));
+		touristoEngine.step(player, "no");
+		assertEquals("Okay, see you around.", getReply(Touristo));
+    }
 
 }
